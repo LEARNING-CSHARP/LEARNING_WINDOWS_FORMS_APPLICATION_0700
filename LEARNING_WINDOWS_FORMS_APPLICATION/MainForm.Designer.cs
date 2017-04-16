a8468e5957d3ed8108d41155b266a1bf051e741e
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2017-04-16 17:52:44 +0430

    Add project files.

---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	partial class MainForm
+	{
+		private System.ComponentModel.IContainer components = null;
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			this.button13 = new System.Windows.Forms.Button();
+			this.button14 = new System.Windows.Forms.Button();
+			this.button15 = new System.Windows.Forms.Button();
+			this.button16 = new System.Windows.Forms.Button();
+			this.button17 = new System.Windows.Forms.Button();
+			this.button18 = new System.Windows.Forms.Button();
+			this.button19 = new System.Windows.Forms.Button();
+			this.button20 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button1.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button2.Location = new System.Drawing.Point(12, 41);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button2.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button3.Location = new System.Drawing.Point(12, 70);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button3.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button4.Location = new System.Drawing.Point(12, 99);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button4.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button5.Location = new System.Drawing.Point(12, 128);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button5.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button6.Location = new System.Drawing.Point(93, 128);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 23);
+			this.button6.TabIndex = 9;
+			this.button6.Text = "button6";
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button6.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button7.Location = new System.Drawing.Point(93, 99);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(75, 23);
+			this.button7.TabIndex = 8;
+			this.button7.Text = "button7";
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button7.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button8.Location = new System.Drawing.Point(93, 70);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(75, 23);
+			this.button8.TabIndex = 7;
+			this.button8.Text = "button8";
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button8.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button9
+			// 
+			this.button9.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button9.Location = new System.Drawing.Point(93, 41);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(75, 23);
+			this.button9.TabIndex = 6;
+			this.button9.Text = "button9";
+			this.button9.UseVisualStyleBackColor = false;
+			this.button9.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button9.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button10
+			// 
+			this.button10.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button10.Location = new System.Drawing.Point(93, 12);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(75, 23);
+			this.button10.TabIndex = 5;
+			this.button10.Text = "button10";
+			this.button10.UseVisualStyleBackColor = false;
+			this.button10.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button10.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button11
+			// 
+			this.button11.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button11.Location = new System.Drawing.Point(174, 128);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(75, 23);
+			this.button11.TabIndex = 14;
+			this.button11.Text = "button11";
+			this.button11.UseVisualStyleBackColor = false;
+			this.button11.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button11.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button12
+			// 
+			this.button12.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button12.Location = new System.Drawing.Point(174, 99);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(75, 23);
+			this.button12.TabIndex = 13;
+			this.button12.Text = "button12";
+			this.button12.UseVisualStyleBackColor = false;
+			this.button12.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button12.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button13
+			// 
+			this.button13.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button13.Location = new System.Drawing.Point(174, 70);
+			this.button13.Name = "button13";
+			this.button13.Size = new System.Drawing.Size(75, 23);
+			this.button13.TabIndex = 12;
+			this.button13.Text = "button13";
+			this.button13.UseVisualStyleBackColor = false;
+			this.button13.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button13.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button14
+			// 
+			this.button14.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button14.Location = new System.Drawing.Point(174, 41);
+			this.button14.Name = "button14";
+			this.button14.Size = new System.Drawing.Size(75, 23);
+			this.button14.TabIndex = 11;
+			this.button14.Text = "button14";
+			this.button14.UseVisualStyleBackColor = false;
+			this.button14.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button14.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button15
+			// 
+			this.button15.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button15.Location = new System.Drawing.Point(174, 12);
+			this.button15.Name = "button15";
+			this.button15.Size = new System.Drawing.Size(75, 23);
+			this.button15.TabIndex = 10;
+			this.button15.Text = "button15";
+			this.button15.UseVisualStyleBackColor = false;
+			this.button15.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button15.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button16
+			// 
+			this.button16.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button16.Location = new System.Drawing.Point(255, 128);
+			this.button16.Name = "button16";
+			this.button16.Size = new System.Drawing.Size(75, 23);
+			this.button16.TabIndex = 19;
+			this.button16.Text = "button16";
+			this.button16.UseVisualStyleBackColor = false;
+			this.button16.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button16.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button17
+			// 
+			this.button17.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button17.Location = new System.Drawing.Point(255, 99);
+			this.button17.Name = "button17";
+			this.button17.Size = new System.Drawing.Size(75, 23);
+			this.button17.TabIndex = 18;
+			this.button17.Text = "button17";
+			this.button17.UseVisualStyleBackColor = false;
+			this.button17.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button17.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button18
+			// 
+			this.button18.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button18.Location = new System.Drawing.Point(255, 70);
+			this.button18.Name = "button18";
+			this.button18.Size = new System.Drawing.Size(75, 23);
+			this.button18.TabIndex = 17;
+			this.button18.Text = "button18";
+			this.button18.UseVisualStyleBackColor = false;
+			this.button18.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button18.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button19
+			// 
+			this.button19.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button19.Location = new System.Drawing.Point(255, 41);
+			this.button19.Name = "button19";
+			this.button19.Size = new System.Drawing.Size(75, 23);
+			this.button19.TabIndex = 16;
+			this.button19.Text = "button19";
+			this.button19.UseVisualStyleBackColor = false;
+			this.button19.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button19.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// button20
+			// 
+			this.button20.BackColor = System.Drawing.Color.PapayaWhip;
+			this.button20.Location = new System.Drawing.Point(255, 12);
+			this.button20.Name = "button20";
+			this.button20.Size = new System.Drawing.Size(75, 23);
+			this.button20.TabIndex = 15;
+			this.button20.Text = "button20";
+			this.button20.UseVisualStyleBackColor = false;
+			this.button20.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+			this.button20.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.SteelBlue;
+			this.ClientSize = new System.Drawing.Size(345, 163);
+			this.Controls.Add(this.button16);
+			this.Controls.Add(this.button17);
+			this.Controls.Add(this.button18);
+			this.Controls.Add(this.button19);
+			this.Controls.Add(this.button20);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button12);
+			this.Controls.Add(this.button13);
+			this.Controls.Add(this.button14);
+			this.Controls.Add(this.button15);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Main";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+		private System.Windows.Forms.Button button13;
+		private System.Windows.Forms.Button button14;
+		private System.Windows.Forms.Button button15;
+		private System.Windows.Forms.Button button16;
+		private System.Windows.Forms.Button button17;
+		private System.Windows.Forms.Button button18;
+		private System.Windows.Forms.Button button19;
+		private System.Windows.Forms.Button button20;
+	}
+}
---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -0,0 +1,36 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_MouseEnter(object sender, System.EventArgs e)
+		{
+			// دکمه جاری که با ماوس روی آن رفته‌ایم
+			System.Windows.Forms.Button
+				currentButton = sender as System.Windows.Forms.Button;
+
+			if (currentButton != null)
+			{
+				currentButton.BackColor =
+					System.Drawing.Color.Yellow;
+			}
+		}
+
+		private void Button_MouseLeave(object sender, System.EventArgs e)
+		{
+			// دکمه جاری که با ماوس از روی آن خارج شده‌ایم
+			System.Windows.Forms.Button
+				currentButton = sender as System.Windows.Forms.Button;
+
+			if (currentButton != null)
+			{
+				currentButton.BackColor =
+					System.Drawing.Color.PapayaWhip;
+			}
+		}
+	}
+}
